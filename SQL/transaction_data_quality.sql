eb3722a7221b20ca2d623e2ca956d272452e8f5a
Lara <larawiede@gmail.com>
2026-06-17 16:51:04 +0200

    Update transaction_data_quality.sql
    primary key validation

--- SQL/transaction_data_quality.sql
+++ SQL/transaction_data_quality.sql
@@ -9,3 +9,14 @@
 
 SELECT COUNT(*) AS total_transactions
 FROM `le-wagon-bootcamp-493213.neo_bank.transactions`;
+-- -----------------------------------------------------
+-- Transaction ID Validation
+-- Check if transaction_id can be used as a primary key
+-- -----------------------------------------------------
+
+SELECT
+    transaction_id,
+    COUNT(*) AS occurrences
+FROM `le-wagon-bootcamp-493213.neo_bank.transactions`
+GROUP BY transaction_id
+HAVING COUNT(*) > 1;
