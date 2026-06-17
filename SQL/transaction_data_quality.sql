637f5746c69f554f7bc67431d05e9de5eb61df71
Lara <larawiede@gmail.com>
2026-06-17 16:42:00 +0200

    Update transaction_data_quality.sql
    Add dataset overview query

--- SQL/transaction_data_quality.sql
+++ SQL/transaction_data_quality.sql
@@ -3,3 +3,9 @@
 -- Neo-Bank Retention & Engagement Project
 -- Author: Lara Wiede
 -- =====================================================
+-- -----------------------------------------------------
+-- Dataset Overview
+-- -----------------------------------------------------
+
+SELECT COUNT(*) AS total_transactions
+FROM `le-wagon-bootcamp-493213.neo_bank.transactions`;
